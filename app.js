64559133e70d220b68af152fd16145937df3a837
Tim Kelley <timkelley683@gmail.com>
2023-04-22 18:30:42 -0700

    ADD: logic for calculator

--- app.js
+++ app.js
@@ -2,44 +2,111 @@
 let weight = document.getElementById("weight");
 let heightFt = document.getElementById("height-ft");
 let heightIn = document.getElementById("height-in");
-let button = document.getElementById("button")
-
-let radioBtn = document.getElementsByClassName("radio-button")
-console.log(radioBtn)
+let submit = document.getElementById("submit")
 
 const form = document.querySelector('form');
 
-function getBMI(weight, heightFt){
-    let BMI = weight / heightFt;
-    console.log(BMI)
+let maleBodyType = document.getElementsByClassName("body-type-image");
+let maleBodyTypeRadio = document.getElementsByClassName("body-type-radio");
+
+//causing an issue with auto submit after changing listener to load
+function removeImageEventOnClick(){
+    for(let i = 0; i < maleBodyType.length; i++){
+        maleBodyType[i].addEventListener('load', function(e){
+            e.preventDefault();
+            console.log("BODY TYPE", maleBodyType[i].value);
+        })
+    }
+}
+
+removeImageEventOnClick();
+
+
+let radioBtn = document.getElementsByClassName("radio-button")
+
+let dailyIntake; 
+
+function coffeeIntake(){
+    for(let i = 0; i < radioBtn.length; i++){
+        if(radioBtn[i].checked){
+            dailyIntake = radioBtn[i].value;
+            
+            console.log(dailyIntake)
+
+            break;
+        }
+    }  
+}
+
+let bodyTypeSelection;
+
+function getBodyType(){
+    for(let i = 0; i < maleBodyTypeRadio.length; i++){
+        if(maleBodyTypeRadio[i].checked){
+            bodyTypeSelection = maleBodyTypeRadio[i].value;
+            console.log(bodyTypeSelection);
+            return bodyTypeSelection
+            break;
+        }
+    }
+    
+
+    for(let i = 0; i < maleBodyType.length; i++){
+        maleBodyType[i].addEventListener('click', (e) => {
+            e.preventDefault();
+            maleBodyTypeRadio[i].checked = true;
+            console.log("CHECKED", maleBodyTypeRadio[i].value)
+        });
+    }
+}
+
+let BMI; 
+
+function getBMI(){
+    let totalHeight = Number((heightFt.value * 12)) + Number(heightIn.value)
+    let heightSquared = totalHeight ** 2;
+
+    BMI = Math.floor(weight.value / heightSquared * 703) + '%';
+    console.log(BMI);
     return BMI;
 }
-button.addEventListener('click', function(e){
+
+form.addEventListener('submit', function(e){
     e.preventDefault();
-    //code for BMI
-    let totalHeight = heightFt.value + (".") + heightIn.value
-    console.log(totalHeight);
-    console.log("Hello")
-    console.log(weight.value)
-    console.log(heightFt.value)
-    let BMI2 = weight.value / totalHeight;
-
-    //Code for coffe intake
-    for(let i = 0; i < radioBtn.length; i++){
-        if(radioBtn.checked = 'true'){
-            let dailyIntake = radioBtn[i].value;
-            console.log(dailyIntake)
-        }
+    console.log(e)
+    // for(let i = 0; i < maleBodyType.length; i++){
         
+    //     if(maleBodyType[i].clicked){
+        
+    //     console.log("BODY TYPES", maleBodyType[i].value);
+    //     break;
+    //     }
+    
+    // }
 
-    }
-    console.log(BMI2);    
-})
+    //CORRECT CODE FOR BODY TYPE
+    // for(let i = 0; i < maleBodyTypeRadio.length; i++){
+    //     if(maleBodyTypeRadio[i].checked){
+    //         bodyTypeSelection = maleBodyTypeRadio[i].value;
+    //         console.log(bodyTypeSelection);
+    //         break;
+    //     }
+    // }
+    
 
-form.addEventListener("submit", (e) => {
-    // e.preventDefault();
-    console.log("SUBMIT")
-})
+    // for(let i = 0; i < maleBodyType.length; i++){
+    //     maleBodyType[i].addEventListener('click', (e) => {
+    //         e.preventDefault();
+    //         maleBodyTypeRadio[i].checked = true;
+    //         console.log("CHECKED", maleBodyTypeRadio[i].value)
+    //     });
+    // }
+    
 
+    getBMI();
+    // bodyTypeRadio();
+    coffeeIntake();
+    getBodyType();
 
-
+    alert(`Your BMI is ${BMI} and body is ${bodyTypeSelection}. Intake is ${dailyIntake}`)
+});
