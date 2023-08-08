b10bf667424fdfeecacc5959a89cbb6c01b4582b
Tim Kelley <timkelley683@gmail.com>
2023-08-07 20:43:04 -0600

    calculator additions

--- app.js
+++ app.js
@@ -5,10 +5,6 @@ let heightIn = document.getElementById("height-in");
 let submit = document.getElementById("submit")
 let baseDose = 150;
 
-
-
-console.log(baseDose)
-
 const form = document.querySelector('form');
 
 let maleBodyType = document.getElementsByClassName("body-type-image");
@@ -36,7 +32,8 @@ function coffeeIntake(){
         if(radioBtn[i].checked){
             dailyIntake = radioBtn[i].value;
             
-            console.log(dailyIntake)
+            // console.log(dailyIntake)
+            return dailyIntake;
 
             break;
         }
@@ -75,50 +72,45 @@ function getBMI(){
     console.log(BMI);
     return BMI;
 }
-if(BMI >= 26.5) {
-    baseDose + 25;
+
+function ectomorphStart(BMI, bodyType, coffeeIntake){
+    // console.log("BMIIII", BMI)
+    // console.log("BMIIII", bodyTypeSelection)
+    // console.log("BMIIII", dailyIntake)
+
+    if( BMI < 26.5 && bodyType == "ectomorph" && coffeeIntake == "1 cup a week or less" ){
+        // console.log(BMI, bodyTypeSelection, dailyIntake)
+        console.log("BMIIII", BMI)
+        console.log("BMIIII", bodyTypeSelection)
+        console.log("BMIIII", dailyIntake)
+        return (BMI, bodyType, coffeeIntake);
+    }
+    return 0
+}
+let profile;
+
+function checkType(profile){
+
 }
 
-// function baseDose(baseDose){
-//     return BMI < 26.5 ? baseDose : baseDose += 25;
-// }
 form.addEventListener('submit', function(e){
     e.preventDefault();
     console.log(e)
-    // for(let i = 0; i < maleBodyType.length; i++){
-        
-    //     if(maleBodyType[i].clicked){
-        
-    //     console.log("BODY TYPES", maleBodyType[i].value);
-    //     break;
-    //     }
     
+    let BMIValue = getBMI();
+    let dailyIntakeValue = coffeeIntake();
+    let bodyTypeValue = getBodyType();
+    console.log("BMIVALUE", BMIValue)
+    console.log("BMIVALUE", dailyIntakeValue)
+    console.log("BMIVALUE", bodyTypeValue)
+
+    let ectomorphStartValue = ectomorphStart(BMIValue, bodyTypeValue, dailyIntakeValue);
+    return (ectomorphStartValue, BMIValue, dailyIntakeValue, bodyTypeValue)
+
+    // function checkProfile(profile){
+    //     return profile ? 1 : "FuCk YeA"
     // }
-
-    //CORRECT CODE FOR BODY TYPE
-    // for(let i = 0; i < maleBodyTypeRadio.length; i++){
-    //     if(maleBodyTypeRadio[i].checked){
-    //         bodyTypeSelection = maleBodyTypeRadio[i].value;
-    //         console.log(bodyTypeSelection);
-    //         break;
-    //     }
-    // }
-    
-
-    // for(let i = 0; i < maleBodyType.length; i++){
-    //     maleBodyType[i].addEventListener('click', (e) => {
-    //         e.preventDefault();
-    //         maleBodyTypeRadio[i].checked = true;
-    //         console.log("CHECKED", maleBodyTypeRadio[i].value)
-    //     });
-    // }
-    
-
-    getBMI();
-    // bodyTypeRadio();
-    coffeeIntake();
-    getBodyType();
-
+    // checkProfile();
     // let intake = `${dailyIntake}`
     // let bmi = `${BMI}`
     // console.log("BMIIII", BMI)
@@ -132,15 +124,17 @@ form.addEventListener('submit', function(e){
     // }
     if(BMI >= 26.5) baseDose += 25;
 
-    if(  dailyIntake === "1 cup a week or less" || dailyIntake === "1 cup daily" && bodyTypeSelection === "ectomorph"){
-        console.log(`1 Your BMI is ${BMI}%. We suggest ${baseDose}mg.`) //150mg
-    }else if( dailyIntake === "1 cup daily" && bodyTypeSelection === "mesomorph" || "endomorph"){
-        console.log(`2 Your BMI is ${BMI}%. We suggest ${(baseDose + 25)}mg.`) //175mg
-    }else if( dailyIntake === "2 cups daily" && bodyTypeSelection === "ectomorph" || "mesomorph"){
-        console.log(`3 Your BMI is ${BMI}%. We suggest ${(baseDose + 25)}mg.`) //175mg
-    }else if( dailyIntake === "2 cups daily" && bodyTypeSelection === "endomorph" || dailyIntake === "3-4 cups daily" && bodyTypeSelection === "ectomorph" || "endomorph"){
-        console.log(`4 Your BMI is ${BMI}%. We suggest ${(baseDose + 50)}mg.`) //200mg
-    }
+    // if(  dailyIntake === "1 cup a week or less" || dailyIntake === "1 cup daily" && bodyTypeSelection === "ectomorph"){
+    //     console.log(`1 Your BMI is ${BMI}%. We suggest ${baseDose}mg.`) //150mg
+    // }else if( dailyIntake === "1 cup daily" && bodyTypeSelection === "mesomorph" || "endomorph"){
+    //     console.log(`2 Your BMI is ${BMI}%. We suggest ${(baseDose + 25)}mg.`) //175mg
+    // }else if( dailyIntake === "2 cups daily" && bodyTypeSelection === "ectomorph" || "mesomorph"){
+    //     console.log(`3 Your BMI is ${BMI}%. We suggest ${(baseDose + 25)}mg.`) //175mg
+    // }else if( dailyIntake === "2 cups daily" && bodyTypeSelection === "endomorph" || dailyIntake === "3-4 cups daily" && bodyTypeSelection === "ectomorph" || "endomorph"){
+    //     console.log(`4 Your BMI is ${BMI}%. We suggest ${(baseDose + 50)}mg.`) //200mg
+    // }
+
+    
     console.log("INTAKE", dailyIntake)
     console.log("BODYTYPEEE", bodyTypeSelection)
     alert(`Your BMI is ${BMI}% and body is ${bodyTypeSelection}. Intake is ${dailyIntake}`)
